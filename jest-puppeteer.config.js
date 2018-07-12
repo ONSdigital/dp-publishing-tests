3fbfbfa5ee403cb8fa57db3bbbf75896a9b35b91
Crispioso <crispin.merriman@gmail.com>
2018-07-12 14:35:22 +0100

    Add HEADLESS environment variable

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     launch: {
         dumpio: process.env.DEBUG === 'true' || process.env.DEBUG === 'puppeteer:*',
-        headless: process.env.DEBUG !== 'true' && process.env.DEBUG !== 'puppeteer:*',
+        headless: (process.env.DEBUG !== 'true' && process.env.DEBUG !== 'puppeteer:*') && (process.env.HEADLESS !== 'false'),
         slowMo: (process.env.DEBUG === 'true' || process.env.DEBUG === 'puppeteer:*') ? 30 : 0,
         timeout: (process.env.DEBUG === 'true' || process.env.DEBUG === 'puppeteer:*') ? 50000 : 30000,
     }
