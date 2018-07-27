df6a0fc3d4a4fc4e98b2e9c299b62de20ea7fe5c
Jon Dewi Jones <jondewijones@gmail.com>
2018-07-27 12:33:19 +0100

    Add tests for preview

--- src/features/preview/preview.test.js
+++ src/features/preview/preview.test.js
@@ -1,16 +1,129 @@
 import expectPuppeteer from 'expect-puppeteer';
 
+import CollectionsPage from '../../pages/collections/CollectionsPage';
+import Zebedee from '../../../clients/Zebedee'
+
 import Page from "../../pages/Page";
 import PreviewPage from "../../pages/preview/PreviewPage";
 
-describe("Previewing a collection", () => {
+const tempCollectionData = [
+    {
+        name: "Acceptance test collection for preview test",
+        publishDate: null,
+        releaseUri: null,
+        teams: [],
+        type: "manual",
+        collectionOwner: "ADMIN"
+    },
+];
+
+const tempPageData = {
+    description: {
+        title: "Test page for collection details acceptance tests",
+        summary: "",
+        releaseDate: "2018-07-05T11:54:32.259Z",
+        keywords: [],
+        metaDescription: ""
+    },
+    markdown: [],
+    charts: [],
+    tables: [],
+    equations: [],
+    images: [],
+    downloads: [],
+    type: "static_page",
+    anchors: [],
+    links: [],
+    fileName: "testpageforcollectiondetailsacceptancetests",
+    uri: "/aboutus/testpageforcollectiondetailsacceptancetests"
+}
+
+const tempTeams = [
+    {name: "AccTest Team 1"},
+    {name: "AccTest Team 2"}
+]
+
+let testCollections = []
+let testPage = {}
+
+describe("Previewing a collection with access", () => {
     beforeAll(async () => {
         await Page.initialise(true);
+        testCollections = await CollectionsPage.setupCollectionsList(tempCollectionData);
+        await Zebedee.createTeam(tempTeams[0].name);
+        await Zebedee.addTeamToCollection(tempTeams[0].name, testCollections[0].id)
+        await Zebedee.addUserToTeam(tempTeams[0].name, Zebedee.getTempViewerUserEmail())
+        testPage = await Zebedee.createPage(testCollections[0].id, tempPageData);
         await Page.loginAsViewer();
     });
 
+    beforeEach(async () => {
+        await CollectionsPage.load();
+        await CollectionsPage.waitForLoad();
+    });
+
+    afterAll(async () => {
+        await CollectionsPage.cleanupCreatedCollections();
+        await Zebedee.deleteTeam(tempTeams[0].name);
+        await Zebedee.deleteTeam(tempTeams[1].name);
+    });
+
     it("loads", async () => {
-        await PreviewPage.load();
+        await PreviewPage.load(testCollections[0].id);
         await expectPuppeteer(page).toMatchElement('#iframe');
     });
+
+    it("user with access to collections can see collections they have access to", async () => {
+        const collections = await CollectionsPage.getAllCollectionsInList();
+        expect(collections.length).toBe(1);
+        expect(collections[0].name).toBe(testCollections[0].name);
+    })
+
+    it("clicking a collection loads preview", async () => {
+        expectPuppeteer(page).toClick(`#${testCollections[0].id}`);
+        await expectPuppeteer(page).toMatchElement('#iframe');
+    })
+
+    it("user can select pages and view content in preview", async () => {
+        let iframeTitle;
+        await PreviewPage.load(testCollections[0].id);
+        await expectPuppeteer(page).toMatchElement('#iframe');
+        const frames = await page.frames();
+        expect(await PreviewPage.getIframeSrc()).toBe("/");
+        expect(await Page.currentPath()).toBe(`/florence/collections/${testCollections[0].id}/preview`);
+        iframeTitle = await PreviewPage.iframeContentHasCorrectTitle(await frames[1].content());
+        expect(iframeTitle).toBe("Home");
+        await expectPuppeteer(page).toSelect('#preview-select', testPage.description.title);
+        expect(await PreviewPage.getIframeSrc()).toBe(testPage.uri);
+        expect(await Page.currentPath()).toEqual(`/florence/collections/${testCollections[0].id}/preview?url=${testPage.uri}`);
+        await frames[1].waitForSelector('.static_page')
+        iframeTitle = await PreviewPage.iframeContentHasCorrectTitle(await frames[1].content());
+        expect(iframeTitle).toBe(testPage.description.title)
+    })
+
 });
+
+describe("Trying to preview without access", () => {
+    beforeAll(async () => {
+        await Page.initialise(true);
+        testCollections = await CollectionsPage.setupCollectionsList(tempCollectionData);
+        await Page.loginAsViewer();
+    });
+
+    afterAll(async () => {
+        await CollectionsPage.cleanupCreatedCollections();
+    });
+
+    it("preview doesn't load, shows and error and redirects user to collections screen", async () => {
+        await PreviewPage.load(testCollections[0].id);
+        await expectPuppeteer(page).toMatchElement('.notifications__item', { text: 'You do not have persmission to view this data and have been redirected to collections screen' });
+        expect(await CollectionsPage.currentPath()).toBe(`/florence/collections`);
+    });
+
+    it("user with access to no collections see no collections when logging in", async () => {
+        await CollectionsPage.load();
+        await CollectionsPage.waitForLoad();
+        const collections = await CollectionsPage.getAllCollectionsInList();
+        expect(collections.length).toBe(0);
+    })
+})
